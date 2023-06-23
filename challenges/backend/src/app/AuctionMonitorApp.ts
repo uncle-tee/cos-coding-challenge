98abcb0487c48c33af464b1c076a1d79a0d34e50
Oluwatobi Adenekan <oluwatobi.t.adenekan@gmail.com>
2023-06-23 11:34:41 +0200

    feat: 🎸 register implemtations and classes in depens container

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -3,15 +3,31 @@ import { inject, injectable } from 'inversify';
 import { DependencyIdentifier } from './DependencyIdentifiers';
 import { ILogger } from './services/Logger/interface/ILogger';
 import 'reflect-metadata';
+import { ICarOnSaleAuctionProcessor } from './services/CarOnSaleClient/interface/ICarOnSaleAuctionProcessor';
+import * as Process from 'process';
 
 @injectable()
 export class AuctionMonitorApp {
   public constructor(
     @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+    @inject(DependencyIdentifier.AUCTION_SERVICE)
+    private auctionCalculatorService: ICarOnSaleAuctionProcessor,
   ) {}
 
   public async start(): Promise<void> {
     this.logger.log(`Auction Monitor started.`);
+
+    try {
+      let auctionCalculatorResult =
+        await this.auctionCalculatorService.processor();
+      this.logger.log(
+        `ACTIONS TOTAL BIDS RESULT ${auctionCalculatorResult.numberOfAuctions}`,
+      );
+      Process.exit(0);
+    } catch (e) {
+      this.logger.error("Failed to process auctions")
+      Process.exit(-1);
+    }
 
     // TODO: Retrieve auctions and display aggregated information (see README.md)
   }
--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -4,18 +4,29 @@ import { AuctionMonitorApp } from './AuctionMonitorApp';
 import { DependencyIdentifier } from './DependencyIdentifiers';
 import { Logger } from './services/Logger/classes/Logger';
 import { ILogger } from './services/Logger/interface/ILogger';
+import { CarOnSaleClient } from './services/CarOnSaleClient/classes/CarOnSaleClient';
+import { ICarOnSaleClient } from './services/CarOnSaleClient/interface/ICarOnSaleClient';
+import { CarOnSaleAuctionProcessor } from './services/CarOnSaleClient/classes/CarOnSaleAuctionProcessor';
+import { ICarOnSaleAuctionProcessor } from './services/CarOnSaleClient/interface/ICarOnSaleAuctionProcessor';
 
 /*
  * Create the DI container.
  */
 const container = new Container({
   defaultScope: 'Singleton',
+  skipBaseClassChecks: true,
 });
 
 /*
  * Register dependencies in DI environment.
  */
 container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
+container
+  .bind<ICarOnSaleClient>(DependencyIdentifier.CAR_ON_SALE_CLIENT)
+  .to(CarOnSaleClient);
+container
+  .bind<ICarOnSaleAuctionProcessor>(DependencyIdentifier.AUCTION_SERVICE)
+  .to(CarOnSaleAuctionProcessor);
 
 /*
  * Inject all dependencies in the application & retrieve application instance.
