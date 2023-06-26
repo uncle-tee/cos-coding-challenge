65c0f62c01ae6a431b661334c8c7744e9c9e36ea
Oluwatobi Adenekan <oluwatobi.t.adenekan@gmail.com>
2023-06-26 10:25:55 +0200

    refactor: 💡 changed value for AUCTION_SERVICE in DependencyIden

--- challenges/backend/src/app/DependencyIdentifiers.ts
+++ challenges/backend/src/app/DependencyIdentifiers.ts
@@ -1,5 +1,5 @@
 export const DependencyIdentifier = {
   LOGGER: 'logger',
   CAR_ON_SALE_CLIENT: 'car_on_sale_client',
-  AUCTION_SERVICE: 'AUCTION_SERVICE',
+  AUCTION_SERVICE: 'action_service',
 };
